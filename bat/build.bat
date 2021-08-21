8dbb1f8ad8d79bc755945de2e793991af882345c
garryz <chimukuanglan@foxmail.com>
2021-08-21 20:05:45 +0800

    添加windows编译脚本

---
+++ bat/build.bat
@@ -0,0 +1,7 @@
+mkdir %~dp0..\build
+
+cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -H%~dp0..\hive -B%~dp0..\build -G "Visual Studio 16 2019" -T host=x64 -A x64
+
+cmake --build %~dp0..\build --config Release --target ALL_BUILD --clean-first -- /maxcpucount:14
+
+pause
